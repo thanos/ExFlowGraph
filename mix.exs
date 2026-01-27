46920d7f2e9f8a1e1fd7fde62f243b754d0ddc04
thanos <thanosv@gmail.com>
2026-01-27 15:45:39 -0500

    small fixes for teh CI

--- mix.exs
+++ mix.exs
@@ -87,7 +87,7 @@ defmodule ExFlowGraph.MixProject do
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: :test},
       {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.40", only: :dev, runtime: false}
     ]
   end
 
